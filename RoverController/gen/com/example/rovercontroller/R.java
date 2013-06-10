5cb75de6f07b8a511fdc62d8dbe7ef0070eb1c2e
Francisco Pinto <me@f.rancis.co>
2013-06-10 15:44:37 +0200

    Changed layout on the rover controller to fullscreen

--- RoverController/gen/com/example/rovercontroller/R.java
+++ RoverController/gen/com/example/rovercontroller/R.java
@@ -26,9 +26,9 @@ public final class R {
     public static final class id {
         public static final int action_settings=0x7f080004;
         public static final int connectButton=0x7f080001;
-        public static final int ipAddress=0x7f080000;
-        public static final int receivedText=0x7f080003;
-        public static final int scrollView1=0x7f080002;
+        public static final int ipAddress=0x7f080003;
+        public static final int receivedText=0x7f080002;
+        public static final int receivedTextScroll=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
